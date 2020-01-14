8b3ebea9e3ace56b1350c94fc089e5e8b8755a2f
mar-veloper <42553243+mar-veloper@users.noreply.github.com>
2020-01-14 13:20:50 +0100

    ma-2

---
+++ Programming-Foundations/MA2/script.js
@@ -0,0 +1,55 @@
+// question 1
+
+let petArray = ["cat", "cow", "dog"];
+
+// question 2
+
+console.log(petArray[1]);
+
+// question 3
+
+petArray.push("sheep");
+console.table(petArray);
+
+// qeustion 4
+
+console.log(`there are ${petArray.length} items inside the petArray`);
+
+// question 5
+
+let catObject = {
+  name: "Garfield",
+  color: ["orange", "black", "yellow"],
+  age: 40 + "years old"
+};
+
+// question 6
+
+console.log(catObject.age);
+
+// question 7
+
+catArray = [
+  {
+    name: "Garfield",
+    color: ["orange", "black", "yellow"],
+    age: 40 + "years old"
+  }
+];
+
+// question 8
+
+for (let i = 0; i < catArray.length; i++) {
+  const element = catArray[i];
+  console.log(element);
+}
+
+// question 9
+
+function logToConsole(value) {
+  console.log(value);
+}
+
+// question 10
+
+logToConsole("donkey");
